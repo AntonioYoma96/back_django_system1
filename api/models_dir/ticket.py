71c389f2bc30388f21be8ff54f87a123aa6e979e
Felipe Maldonado <felipe.maldonado@sistemasexpertos.cl>
2020-12-04 09:28:58 -0300

    Gestión de etiquetas y mejora de rutas para el modelo Ticket

--- api/models_dir/ticket.py
+++ api/models_dir/ticket.py
@@ -1,22 +1,50 @@
+import os
+from datetime import datetime
+
 from django.db import models
+
+
+def get_image_ticket_path(instance, filename):
+    ticket_path = f'ticket_{instance.ticket.id}'
+    return new_filename(ticket_path, filename)
+
+
+def get_image_message_path(instance, filename):
+    ticket_path = f'mensaje_{instance.ticket.id}'
+    return new_filename(ticket_path, filename)
+
+
+def new_filename(ticket_path, file):
+    today = datetime.now()
+    root = 'image'
+    filename = '{year}_{month}_{day}_{secs}.{extension}'.format(
+        year=today.year,
+        month=today.month,
+        day=today.day,
+        secs=today.microsecond,
+        extension=file.split('.')[-1]
+    )
+    return os.path.join(root, ticket_path, filename)
 
 
 class Ticket(models.Model):
     cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
-    asignado = models.ForeignKey('Colaborador', on_delete=models.CASCADE)
-    solicitante = models.ForeignKey('Colaborador', on_delete=models.CASCADE)
+    asignado = models.ForeignKey('Colaborador', on_delete=models.CASCADE, related_name='ticket_asignado')
+    solicitante = models.ForeignKey('Colaborador', on_delete=models.CASCADE, related_name='ticket_solicitado')
+    validador = models.ForeignKey(
+        'Colaborador',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='ticket_validar'
+    )
     unidad = models.ForeignKey('Unidad', on_delete=models.CASCADE)
-    validador = models.ForeignKey('Colaborador', on_delete=models.CASCADE, blank=True, null=True)
-    version = models.CharField(max_length=10, blank=True, null=True)
     # módulo
+    version = models.CharField(max_length=10, blank=True, null=True)
     integracion = models.CharField('integración', max_length=50, blank=True, null=True)
     prioridad = models.ForeignKey('Prioridad', on_delete=models.CASCADE)
     tipo_ticket = models.ForeignKey('TipoTicket', on_delete=models.CASCADE, verbose_name='tipo de ticket')
     fecha_limite = models.DateField('fecha límite', blank=True, null=True)
-    puntos = models.PositiveSmallIntegerField(blank=True, null=True)
-    esperar_definicion_referencia = models.BooleanField('esperar definición de referencia', blank=True, null=True)
-    is_errror_usuario = models.BooleanField('error de usuario')
-    is_tarea_bloque = models.BooleanField('tarea de bloque')
     ruta = models.URLField(blank=True, null=True)
     asunto = models.CharField(max_length=100)
     descripcion = models.TextField('descripción')
@@ -51,7 +79,7 @@ class EstadoTicket(models.Model):
 
 class ImagenTicket(models.Model):
     ticket = models.ForeignKey('Ticket', on_delete=models.CASCADE)
-    imagen = models.ImageField(upload_to=f'tickets/{ticket.id}/%Y%m%d/')
+    imagen = models.ImageField(upload_to=get_image_ticket_path)
 
     class Meta:
         verbose_name = 'imagen del ticket'
@@ -62,18 +90,20 @@ class Mensaje(models.Model):
     ticket = models.ForeignKey('Ticket', on_delete=models.CASCADE)
     asunto = models.CharField(max_length=100)
     descripcion = models.TextField('descripción')
-    is_error_usuario = models.BooleanField('error de usuario')
-    is_tarea_bloque = models.BooleanField('tarea de bloque')
-    is_falta_informacion = models.BooleanField('falta de información')
-    is_mejorar_redaccion = models.BooleanField('mejorar redacción')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
 
 class ImagenMensaje(models.Model):
     mensaje = models.ForeignKey('Mensaje', on_delete=models.CASCADE)
-    imagen = models.ImageField(upload_to=f'tickets/{mensaje.id}/%Y%m%d/')
+    imagen = models.ImageField(upload_to=get_image_message_path)
 
     class Meta:
         verbose_name = 'imagen del mensaje'
         verbose_name_plural = 'imágenes de los mensajes'
+
+
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=50, unique=50)
+    tickets = models.ManyToManyField('Ticket')
+    mensajes = models.ManyToManyField('Mensaje')
